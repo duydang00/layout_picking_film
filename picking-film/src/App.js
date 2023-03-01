e024e40252d6be6e520732a7fce07a86ee2ffb63
Duy <dangnguyenkhanhduy92@gmail.com>
2023-03-01 23:09:47 +0700

    Layout picking films added

---
+++ picking-film/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+import RenderFilms from './RenderFilms';
+
+function App() {
+
+
+  return (
+    <div className="App">
+      
+      <RenderFilms />
+      
+    </div>
+  );
+}
+
+export default App;
+
